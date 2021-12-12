ad1f2bd24d4a4d8fe8fa37bbf005f887c2ae7c8d
iktefish <87042742+iktefish@users.noreply.github.com>
2021-12-12 10:44:29 +0600

    Adding ts, html, css, diagnostic, emmet servers and exlint_d and prettier to nodeNix

---
+++ node/shell.nix
@@ -0,0 +1,9 @@
+with import <nixpkgs> {};
+let nix-convert = pkgs.writeShellScriptBin ''nix-convert'' ''nix-shell -p nodePackages.node2nix --command "node2nix -i ./node-packages.json -o node-packages.nix"'';
+
+in stdenv.mkDerivation {
+    name = "nodeNix";
+    buildInputs = [
+        nix-convert
+    ];
+}
